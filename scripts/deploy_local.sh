868867ebfe2ad8dd73788a46c2d69fba19960ceb
Yuka Saito <yksaito02@gmail.com>
2022-11-09 19:47:29 +0900

    mod: deploy_local.sh

--- scripts/deploy_local.sh
+++ scripts/deploy_local.sh
@@ -16,9 +16,21 @@ dfx start --clean --background
 dfx deploy GoldDIP20 --argument='("Token Gold Logo", "Token Silver", "TGD", 8, 10000000000000000, principal '\"$ROOT_PRINCIPAL\"', 0)'
 dfx deploy SilverDIP20 --argument='("Token Silver Logo", "Token Silver", "TSV", 8, 10000000000000000, principal '\"$ROOT_PRINCIPAL\"', 0)'
 
-# deploy Internet Identity
+export GoldDIP20_PRINCIPAL=$(dfx canister id GoldDIP20)
+export SilverDIP20_PRINCIPAL=$(dfx canister id SilverDIP20)
+
+# ===== Deploy Internet Identity =====
 dfx deploy internet_identity
 
+# ===== Deploy faucet canister and mint token =====
+dfx deploy faucet
+
+export FAUCET_PRINCIPAL=$(dfx canister id faucet)
+dfx canister call GoldDIP20 mint '(principal '\"$FAUCET_PRINCIPAL\"', 100_000)'
+dfx canister call SilverDIP20 mint '(principal '\"$FAUCET_PRINCIPAL\"', 100_000)'
+
+# ===== Deploy icp_basic_dex_backend canister =====
 dfx deploy icp_basic_dex_backend
 
-dfx deploy  icp_basic_dex_frontend
+# ===== Deploy icp_basic_dex_frontend =====
+dfx deploy icp_basic_dex_frontend
